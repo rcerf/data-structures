ebf61f8b7fb701c5ca5af0ed440e9ce129705d5e
rcerf <rickcerf@gmail.com>
2013-09-25 16:29:44 -0700

    completed pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,4 +1,25 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.queueSize = 0;
+};
+
+
+Queue.prototype.enqueue = function(value){
+  this.queueSize++;
+  for(var i = this.queueSize; i>1; i--){
+    this.storage[i] = this.storage[i-1];
+  }
+  this.storage[1] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  var val = this.storage[this.queueSize];
+  this.queueSize && this.queueSize--;
+  return val;
+};
+
+Queue.prototype.size = function(){
+  return this.queueSize;
 };
 
