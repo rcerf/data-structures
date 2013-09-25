c866361545a4fffc63f5c0be505408fc6596d8c6
rcerf <rickcerf@gmail.com>
2013-09-25 15:12:16 -0700

    finished queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    size++;
+    for(var i = size; i>1; i--){
+      storage[i] = storage[i-1];
+    }
+    storage[1] = value;
   };
 
   instance.dequeue = function(){
+    var val = storage[size];
+    size && size--;
+    return val;
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
