ef33f038f62dca8f28586c087e4ee993956c97cd
rcerf <rickcerf@gmail.com>
2013-09-25 16:13:06 -0700

    protypal stack.js completed

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,28 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.instanceSize = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.instanceSize++;
+    this.storage[this.instanceSize] = value;
+  },
 
+  pop: function(){
+    var val = this.storage[this.instanceSize];
+    this.instanceSize && this.instanceSize--;
+    return val;
+  },
+
+  size: function(){
+    return this.instanceSize;
+  }
+};
