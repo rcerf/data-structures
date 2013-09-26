c51ec80b1fe3ba943d4018b04a00e2f53a69f291
rcerf <rickcerf@gmail.com>
2013-09-25 17:21:20 -0700

    completed pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,4 +1,25 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.instanceSize = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+};
+
+Stack.prototype.push = function(value){
+  this.instanceSize++;
+  this.storage[this.instanceSize] = value;
+};
+
+Stack.prototype.pop = function(){
+  var val = this.storage[this.instanceSize];
+  this.instanceSize && this.instanceSize--;
+  return val;
+};
+
+Stack.prototype.size = function(){
+  return this.instanceSize;
 };
 
