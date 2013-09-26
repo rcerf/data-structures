ff5dcbde956b83aa81704767ae426847e839cdca
rcerf <rickcerf@gmail.com>
2013-09-26 10:53:25 -0700

    modified the makeTree function and created the addChild function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,39 @@
-var makeTree = function(){
+var makeTree = function(value){
   var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
+  newTree.value = value;
+  newTree.children = [];
+  newTree.parent = null;
   _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  var child = makeTree(value);
+  child.parent = this;
+  this.children.push(child);
 };
 
 treeMethods.contains = function(){
 };
+
+
+
+// newTree.childred should starts as [] = each subtree node with have arrays of objects as the children, so it makes sense that when there are NO children, we just find an empty array.
+// Should makeTree initiate the FIRST (most parent) node? 
+// Contains should work recursively, like getElementsByClassName, except it will not need to build an arrays of nodes, it just needs find one, then recurse backwards
+// addChild should make a new tree object, and it to the parent tree's ,children array. It will need to be passed the parent node(?)
+// .contains should return the object, to pass in as a node in making a new child.
+// var thisTree = makeTree(Head);
+
+// var makeTree = function(value) {
+// // node = node || "Head"; (?)
+// newTree.value = value;
+// newTree.children = [];
+
+// treeMethods.addChild = function(node, value) {
+// var addedTo = this.contains(node) {
+// var newTree = makeTree(value);
+// addedTo.children.push(newTree);
+// }
