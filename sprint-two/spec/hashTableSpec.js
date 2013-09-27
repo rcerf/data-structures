1a68b629ded2c7a19f6550edd3def460e9e5d6c4
rcerf <rickcerf@gmail.com>
2013-09-27 16:07:53 -0700

    Testing to make sure .remove() method is functioning in all cases

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -13,21 +13,33 @@ describe("hashTable", function() {
   it("should handle hash function collisions", function(){
     // force the hash function to return 0
     spyOn(window, 'getIndexBelowMaxForKey').andReturn(0);
-    var v1 = 'val1', v2 = 'val2';
+    var v1 = 'val1', v2 = 'val2', v3 = 'val3', v4 = 'val4', v5 = 'val5';
     hashTable.insert(v1, v1);
     hashTable.insert(v2, v2);
+    hashTable.insert(v3, v3);
+    hashTable.insert(v4, v4);
+    hashTable.insert(v5, v5);
     expect(hashTable.retrieve(v1)).toEqual(v1);
     expect(hashTable.retrieve(v2)).toEqual(v2);
+    expect(hashTable.retrieve(v5)).toEqual(v5);
   });
 
-  // add more tests!
-  // it("should be able to remove a key" , function() {
-  //   var v1 = 'val1', v2 = 'val2', v3 = 'val3', v4 = 'val4';
-  //   hashTable.insert(v1, v1);
-  //   hashTable.insert(v2, v2);
-  //   hashTable.insert(v3, v3);
-  //   hashTable.insert(v4, v4);
-  //   hashTable.remove(v3);
-  //   expect(hashTable.retrieve(v3)).toEqual(undefined);
-  // });
+  //add more tests!
+  it("should be able to remove a key" , function() {
+    var v1 = 'val1', v2 = 'val2', v3 = 'val3', v4 = 'val4';
+    hashTable.insert(v1, v1);
+    hashTable.insert(v2, v2);
+    hashTable.insert(v3, v3);
+    hashTable.insert(v4, v4);
+    hashTable.remove(v3);
+    expect(hashTable.retrieve(v3)).toEqual(undefined);
+    // force the hash function to return 0
+    spyOn(window, 'getIndexBelowMaxForKey').andReturn(0);
+    hashTable.insert(v1, v1);
+    hashTable.insert(v2, v2);
+    hashTable.insert(v3, v3);
+    hashTable.insert(v4, v4);
+    hashTable.remove(v3);
+    expect(hashTable.retrieve(v3)).toEqual(undefined);
+  });
 });
