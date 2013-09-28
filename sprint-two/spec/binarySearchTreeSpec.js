4ce12864b7f713b8bdf94cc0962873b149d194ad
rcerf <rickcerf@gmail.com>
2013-09-27 21:16:49 -0700

    Added the extra credit BST calls, and started building binarySearchTree methods.

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -6,9 +6,26 @@ describe("binarySearchTree", function() {
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
-    expect(binarySearchTree.children).toEqual(jasmine.any(Function));
+    expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
   // add more tests here to test the functionality of binarySearchTree
+  it("should have a working contains method", function(){
+    debugger;
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(1);
+    //binarySearchTree.insert(3);
+    //binarySearchTree.insert(7);
+    // expect(binarySearchTree.contains(3)).toEqual(true);
+    // expect(binarySearchTree.contains(10)).toEqual(false);
+  });
+
+  // it("should be able to insert a value properly", function(){
+  //   binarySearchTree.insert(5);
+  //   binarySearchTree.insert(1);
+  //   binarySearchTree.insert(3);
+  //   binarySearchTree.insert(7);
+  //   expect(binarySearchTree.leftmost()).toEqual(1);
+  // });
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,57 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(value){
+  var binarySearchTree = {
+    left: null,
+    right: null,
+    value: value
+  };
+
+  binarySearchTree = extend(binarySearchTree, binarySearchTreeMethods);
+  return binarySearchTree;
+};
+
+var extend = function(destination, source) {
+  for (var key in source) {
+    destination[key] = source[key];
+  }
+  return destination;
+};
+
+var binarySearchTreeMethods = {
+  insert: function(value, node){
+    var newNode;
+    node = node || this;
+    debugger
+    if(!this.value){
+      this.value = value;
+    }else if(this.value < value){
+      if(this.right){
+        this.insert(value, this.right);
+      }else{
+        newNode = makeBinarySearchTree(value);
+        this.right = newNode;
+      }
+    }else{
+      if(this.left){
+        this.insert(value, this.left);
+      }else{
+        newNode = makeBinarySearchTree(value);
+        this.left = newNode;
+      }
+    }
+  },
+
+  contains: function(value, node){
+    node = node || this;
+    var present = false;
+    debugger;
+    if(this.value > value){
+      present = this.contains(value, this.left);
+    }else if(this.value < value){
+      present = this.contains(value, this.right);
+    }else{
+      present = true;
+    }
+    return present;
+  },
+  depthFirstLog: function(){}
 };
