1580b7b7dac1661d0b22bbb9c34e486ffb2e65c1
rcerf <rickcerf@gmail.com>
2013-09-28 18:24:18 -0700

    Sets will now take numbers, arrays and objects in addition to strings, and treat them all as separate.

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -12,13 +12,13 @@ describe("set", function() {
   });
 
   it("should add to the set", function(){
-    var set = makeSet();
-    set.add("Hello");
-    expect(set._storage["Hello"]).toEqual(true);
+    var string = "Hello";
+    set.add(string);
+    var key = set.hashIt(string)
+    expect(set._storage[key]).toEqual("Hello");
   });
 
   it('should determine whether an object is present.', function() {
-    var set = makeSet();
     set.add("Hello");
     set.add("World");
     set.add("Dog");
@@ -26,12 +26,36 @@ describe("set", function() {
   });
 
   it("should remove an element from the set", function() {
-    var set = makeSet();
     set.add("Hello");
     set.add("World");
     set.add("Dog");
     set.remove("World");
     expect(set.contains("World")).toEqual(false);
   });
-  
+
+  it("should be able to handle numbers as well as string", function() {
+    set.add(6);
+    set.add("Hello");
+    set.add("six");
+    set.add("3");
+    set.add(3);
+    set.add(1);
+    set.remove(3);
+    expect(set.contains("3")).toEqual(true);
+  });
+
+  it("should handle any stupid thing I throw at it", function() {
+    set.add({a:"hello", b:"goodbye"});
+    set.add(3);
+    set.add("3");
+    set.add("three");
+    set.add([1,2,3]);
+    set.add("[123]");
+    set.add('{}');
+    set.remove("3");
+    expect(set.contains(3)).toEqual(true);
+    var arr = [1,2,3];
+    set.remove(arr);
+    expect(set.contains([1,2,3])).toEqual(false);
+  });
 });
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,15 +7,44 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(value){
-  if (!this._storage[value]) {
-    this._storage[value] = true;
+  key = this.hashIt(value);
+  if (!this._storage[key]) {
+    this._storage[key] = value;
   }
 };
 
+setPrototype.hashIt = function(value) {
+  var key = "";
+  if (typeof value === 'string') {
+    key = "'" + value + "'";
+  } else if (typeof value === 'number') {
+    key = "#" + value;
+  } else if (Array.isArray(value)) {
+    key += "[";
+    for (var i = 0; i < value.length; i++) {
+      key += value[i];
+    }
+    key += "]";
+  } else {
+    key += "{";
+    for (var prop in value) {
+      key += prop + ': ';
+      key += value[prop] + " ";
+    }
+    key += "}";
+  }
+  return key;
+};
+
 setPrototype.contains = function(value){
-  return !!this._storage[value];
+  key = this.hashIt(value);
+  if (this._storage[key] === undefined) {
+    return false;
+  }
+  return true;
 };
 
 setPrototype.remove = function(value){
-  delete this._storage[value];
+  key = this.hashIt(value);
+  delete this._storage[key];
 };
